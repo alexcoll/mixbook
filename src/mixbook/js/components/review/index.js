8939035ab4637532e64d6fb528fc06a87cb191f6
nzenobi <nzenobi@purdue.edu>
2017-04-12 19:22:57 -0400

    Update index.js

--- src/mixbook/js/components/review/index.js
+++ src/mixbook/js/components/review/index.js
@@ -346,7 +346,7 @@ class Reviews extends Component {
                 <Input inlineLabel label="Review"
                 placeholder="Enter a review "
                 value={this.state.reviews}
-                onChangeText={review => this.setState({this.state.reviews})}
+                onChangeText={review => this.setState({reviews})}
                 />
               </InputGroup>
             </ListItem>
