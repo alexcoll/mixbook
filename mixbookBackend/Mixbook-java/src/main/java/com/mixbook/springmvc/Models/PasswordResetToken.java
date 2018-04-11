11dcf3606b204b7a1d534b99e8df78f1a8c33097
John Tyler Preston <prestoj@purdue.edu>
2018-04-11 00:12:57 -0400

    Removed unnecessary import from model class

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Models/PasswordResetToken.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Models/PasswordResetToken.java
@@ -14,7 +14,6 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="password_reset_token")
