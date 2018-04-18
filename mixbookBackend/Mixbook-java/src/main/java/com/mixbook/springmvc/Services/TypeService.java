fd4d6a7dcfdf2aa0b5d7072a0324b67909803507
John Tyler Preston <prestoj@purdue.edu>
2018-04-18 12:38:24 -0400

    Added missing documentation to TypeService

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Services/TypeService.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Services/TypeService.java
@@ -5,6 +5,11 @@ import java.util.List;
 import com.mixbook.springmvc.Exceptions.UnknownServerErrorException;
 import com.mixbook.springmvc.Models.Type;
 
+/**
+ * Interface to provide modular service layer functionality for type related tasks for the controller layer.
+ * @author John Tyler Preston
+ * @version 1.0
+ */
 public interface TypeService {
 
 	/**
