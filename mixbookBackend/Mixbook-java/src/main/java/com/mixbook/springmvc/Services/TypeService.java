fab626290be7987a77281cac72d4dd066bc77b3f
John Tyler Preston <prestoj@purdue.edu>
2018-04-18 12:34:41 -0400

    Finished documenting TypeService

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Services/TypeService.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Services/TypeService.java
@@ -7,6 +7,11 @@ import com.mixbook.springmvc.Models.Type;
 
 public interface TypeService {
 
+	/**
+	 * Loads a complete list of types of styles of ingredients.
+	 * @return a complete list of types of styles of ingredients.
+	 * @throws UnknownServerErrorException the exception is thrown when an unknown server error occurs.
+	 */
 	List<Type> getTypes() throws UnknownServerErrorException;
 
 }
