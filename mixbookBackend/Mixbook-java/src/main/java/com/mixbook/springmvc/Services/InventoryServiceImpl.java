8f523929d7d0c38e9526d121d6c302d8f4821438
John Tyler Preston <prestoj@purdue.edu>
2018-04-18 19:48:44 -0400

    Finished documenting InventoryServiceImpl

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Services/InventoryServiceImpl.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Services/InventoryServiceImpl.java
@@ -17,10 +17,18 @@ import com.mixbook.springmvc.Exceptions.UnknownServerErrorException;
 import com.mixbook.springmvc.Models.Brand;
 import com.mixbook.springmvc.Models.User;
 
+/**
+ * Provides the concrete implementation of the modular service layer functionality for inventory related tasks for the controller layer.
+ * @author John Tyler Preston
+ * @version 1.0
+ */
 @Service("inventoryService")
 @Transactional
 public class InventoryServiceImpl implements InventoryService {
 
+	/**
+	 * Provides ability to access account unlock data layer functions.
+	 */
 	@Autowired
 	private InventoryDao dao;
 
