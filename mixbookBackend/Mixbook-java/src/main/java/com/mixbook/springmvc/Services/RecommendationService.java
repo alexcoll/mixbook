fdd43a147e95bc49543509f0e014af8ddfd2f98d
John Tyler Preston <prestoj@purdue.edu>
2018-04-18 12:31:29 -0400

    Finished documenting RecommendationService

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Services/RecommendationService.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Services/RecommendationService.java
@@ -7,11 +7,27 @@ import com.mixbook.springmvc.Models.Recommendation;
 import com.mixbook.springmvc.Models.User;
 
 public interface RecommendationService {
-	
+
+	/**
+	 * Recommends a recipe to another user.
+	 * @param recommendation the recommendation to make.
+	 * @throws UnknownServerErrorException the exception is thrown when an unknown server error occurs.
+	 */
 	void recommendRecipe(Recommendation recommendation) throws UnknownServerErrorException;
-	
+
+	/**
+	 * Deletes a recommendation that a user has received.
+	 * @param recommendation the recommendation to delete.
+	 * @throws UnknownServerErrorException the exception is thrown when an unknown server error occurs.
+	 */
 	void deleteRecommendation(Recommendation recommendation) throws UnknownServerErrorException;
-	
+
+	/**
+	 * Loads a list of recommendations that a user has received.
+	 * @param user the user for whom to load the recommendations.
+	 * @return a list of recommendations that a user has received.
+	 * @throws UnknownServerErrorException the exception is thrown when an unknown server error occurs.
+	 */
 	Set<Recommendation> loadRecommendations(User user) throws UnknownServerErrorException;
 
 }
