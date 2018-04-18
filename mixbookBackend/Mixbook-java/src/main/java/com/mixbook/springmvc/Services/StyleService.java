75bfd10a81fef0c8dfc35770e55e0bb5a162570c
John Tyler Preston <prestoj@purdue.edu>
2018-04-18 12:33:05 -0400

    Finished documenting StyleService

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Services/StyleService.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Services/StyleService.java
@@ -7,6 +7,11 @@ import com.mixbook.springmvc.Models.Style;
 
 public interface StyleService {
 
+	/**
+	 * Loads a complete list of styles of ingredients.
+	 * @return a complete list of styles of ingredients.
+	 * @throws UnknownServerErrorException the exception is thrown when an unknown server error occurs.
+	 */
 	List<Style> getStyles() throws UnknownServerErrorException;
 
 }
