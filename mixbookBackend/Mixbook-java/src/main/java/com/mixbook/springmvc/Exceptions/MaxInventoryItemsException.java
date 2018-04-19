95750a4943ca118876cf2f0a9ca0b4c84d248c3c
John Tyler Preston <prestoj@purdue.edu>
2018-04-19 19:30:28 -0400

    Finished documenting MaxRecipeIngredientsException as well as refactored a couple comments to make them more clear

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Exceptions/MaxInventoryItemsException.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Exceptions/MaxInventoryItemsException.java
@@ -1,14 +1,14 @@
 package com.mixbook.springmvc.Exceptions;
 
 /**
- * Thrown when a user attempts to add too many items to their inventory.
+ * Thrown when a user attempts to add too many ingredients to their inventory.
  * @author John Tyler Preston
  * @version 1.0
  */
 public class MaxInventoryItemsException extends Exception {
 
 	/**
-	 * Constructs an instance of an exception when a user attempts to add too many items to their inventory.
+	 * Constructs an instance of an exception when a user attempts to add too many ingredients to their inventory.
 	 * @param s the exception message to include.
 	 */
 	public MaxInventoryItemsException(String s) {
