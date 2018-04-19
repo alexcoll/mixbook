5c0c43add8f7db27319a69bd6ae41ed571e10a20
John Tyler Preston <prestoj@purdue.edu>
2018-04-18 20:03:37 -0400

    Finished documenting StyleServiceImpl

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Services/StyleServiceImpl.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Services/StyleServiceImpl.java
@@ -11,10 +11,18 @@ import com.mixbook.springmvc.DAO.StyleDao;
 import com.mixbook.springmvc.Exceptions.UnknownServerErrorException;
 import com.mixbook.springmvc.Models.Style;
 
+/**
+ * Provides the concrete implementation of the modular service layer functionality for brand style related tasks for the controller layer.
+ * @author John Tyler Preston
+ * @version 1.0
+ */
 @Service("styleService")
 @Transactional
 public class StyleServiceImpl implements StyleService {
 
+	/**
+	 * Provides ability to access brand style data layer functions.
+	 */
 	@Autowired
 	private StyleDao dao;
 
