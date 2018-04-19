5914c6e8dff3bc7967e8fb51b630ca3018c9408c
John Tyler Preston <prestoj@purdue.edu>
2018-04-18 20:06:26 -0400

    Finished documenting TypeServiceImpl

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Services/TypeServiceImpl.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Services/TypeServiceImpl.java
@@ -11,10 +11,18 @@ import com.mixbook.springmvc.DAO.TypeDao;
 import com.mixbook.springmvc.Exceptions.UnknownServerErrorException;
 import com.mixbook.springmvc.Models.Type;
 
+/**
+ * Provides the concrete implementation of the modular service layer functionality for style type related tasks for the controller layer.
+ * @author John Tyler Preston
+ * @version 1.0
+ */
 @Service("typeService")
 @Transactional
 public class TypeServiceImpl implements TypeService {
 
+	/**
+	 * Provides ability to access style type data layer functions.
+	 */
 	@Autowired
 	private TypeDao dao;
 
