2d71962b692ae271b6a308b48e50d5addc8f161b
John Tyler Preston <prestoj@purdue.edu>
2018-04-19 11:03:24 -0400

    Finished documenting NameFormatter

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Formatters/NameFormatter.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Formatters/NameFormatter.java
@@ -6,6 +6,13 @@ import java.util.Locale;
 import org.springframework.format.Formatter;
 import org.thymeleaf.util.StringUtils;
 
+/**
+ * Name formatter class that implements the Spring Formatter interface.
+ * <p>
+ * Formats a name(String) and returns the value with spaces replaced by commas.
+ * @author John Tyler Preston
+ * @version 1.0
+ */
 public class NameFormatter implements Formatter<String> {
 
 	@Override
@@ -18,6 +25,12 @@ public class NameFormatter implements Formatter<String> {
 		return formatName(input, locale);
 	}
 
+	/**
+	 * Formats a name(String) and returns the value with spaces replaced by commas.
+	 * @param input the string to format.
+	 * @param locale represents the geographical, political, or cultural region to be used for locale-sensitive formatting.
+	 * @return the input with spaces replaced by commas.
+	 */
 	private String formatName(String input, Locale locale) {
 		return StringUtils.replace(input, " ", ",");
 	}
