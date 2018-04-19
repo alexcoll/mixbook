90c2be64bc716e0ae8a92850546d1648b6e99384
John Tyler Preston <prestoj@purdue.edu>
2018-04-19 11:15:30 -0400

    Finished documenting InvalidIngredientException

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Exceptions/InvalidIngredientException.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Exceptions/InvalidIngredientException.java
@@ -1,7 +1,16 @@
 package com.mixbook.springmvc.Exceptions;
 
+/**
+ * Thrown when a user attempts to use an invalid/non-existent ingredient.
+ * @author John Tyler Preston
+ * @version 1.0
+ */
 public class InvalidIngredientException extends Exception {
-	
+
+	/**
+	 * Constructs an instance of an exception when a user attempts to use an invalid/non-existent ingredient.
+	 * @param s the exception message to include.
+	 */
 	public InvalidIngredientException(String s) {
 		super(s);
 	}
