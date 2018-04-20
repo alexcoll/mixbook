73c4f1d2e4de2c4877dbc4fd9cc22ee1c0d3ef47
John Tyler Preston <prestoj@purdue.edu>
2018-04-19 21:39:28 -0400

    Finished documenting WebSecurityConfig as well as added missing period in comment in SecurityWebApplicationInitializer

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Security/SecurityWebApplicationInitializer.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Security/SecurityWebApplicationInitializer.java
@@ -3,7 +3,7 @@ package com.mixbook.springmvc.Security;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 /**
- * Registers the <code>DelegatingFilterProxy</code> to use the <code>springSecurityFilterChain</code> before any other registered <code>Filter</code>
+ * Registers the <code>DelegatingFilterProxy</code> to use the <code>springSecurityFilterChain</code> before any other registered <code>Filter</code>.
  * @author John Tyler Preston
  * @version 1.0
  */
