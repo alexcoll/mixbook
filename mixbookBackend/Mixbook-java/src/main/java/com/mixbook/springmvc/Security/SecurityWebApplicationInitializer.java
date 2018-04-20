faf7764369149b07d476979996979eabb0b90d85
John Tyler Preston <prestoj@purdue.edu>
2018-04-19 21:34:03 -0400

    Finished documenting SecurityWebApplicationInitializer

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Security/SecurityWebApplicationInitializer.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Security/SecurityWebApplicationInitializer.java
@@ -2,6 +2,11 @@ package com.mixbook.springmvc.Security;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+/**
+ * Registers the <code>DelegatingFilterProxy</code> to use the <code>springSecurityFilterChain</code> before any other registered <code>Filter</code>
+ * @author John Tyler Preston
+ * @version 1.0
+ */
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }
