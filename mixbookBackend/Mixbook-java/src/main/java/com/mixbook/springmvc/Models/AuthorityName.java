f63166da8d7f64e14aebc9ad22298b8ccb91aa67
John Tyler Preston <prestoj@purdue.edu>
2018-04-22 19:41:08 -0400

    Finished documenting AuthorityName

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Models/AuthorityName.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/Models/AuthorityName.java
@@ -1,7 +1,20 @@
 package com.mixbook.springmvc.Models;
 
+/**
+ * Describes the permissions role of a user, either a normal user or an administrator.
+ * @author John Tyler Preston
+ * @version 1.0
+ */
 public enum AuthorityName {
 
-	ROLE_USER, ROLE_ADMIN
+	/**
+	 * A permissions role. Indicates a normal user.
+	 */
+	ROLE_USER,
+
+	/**
+	 * A permissions role. Indicates a super user/administrator.
+	 */
+	ROLE_ADMIN
 
 }
