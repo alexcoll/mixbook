a427b83e7c06831b66b52e72cc4b1a24dee897f2
John Tyler Preston <prestoj@purdue.edu>
2018-04-23 21:25:58 -0400

    Finished documenting TypeDao

--- mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/DAO/TypeDao.java
+++ mixbookBackend/Mixbook-java/src/main/java/com/mixbook/springmvc/DAO/TypeDao.java
@@ -4,8 +4,18 @@ import java.util.List;
 
 import com.mixbook.springmvc.Models.Type;
 
+/**
+ * Interface to provide modular data layer functionality for type related tasks for the service layer.
+ * @author John Tyler Preston
+ * @version 1.0
+ */
 public interface TypeDao {
 
+	/**
+	 * Loads a complete list of types of styles of ingredients.
+	 * @return a complete list of types of styles of ingredients.
+	 * @throws Exception the exception is thrown when an unknown server error occurs.
+	 */
 	List<Type> getTypes() throws Exception;
 
 }
